d9a89f91da02dfe97dc2f8dc05db9ac44dcfdba9
Congshan Liu <rhexliu@gmail.com>
2018-09-17 10:19:05 +0800

    add harbor

--- vm_env.sh
+++ vm_env.sh
@@ -1,3 +1,3 @@
 export http_proxy=http://10.0.2.2:8118
 export https_proxy=http://10.0.2.2:8118
-export no_proxy="test.local.com,docker.local.com,localhost,127.0.0.1,192.168.99.100,192.168.56.101",$no_proxy
+export no_proxy="test.local.com,docker.local.com,localhost,127.0.0.1,192.168.99.100,192.168.56.101,harbor.mydomain.com",$no_proxy
